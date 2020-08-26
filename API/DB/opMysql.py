14c4027392a4020890b1cb0ad12d1998183c15bb
T8840 <qinmimi100@126.com>
2020-08-26 09:50:30 +0800

    add opMysql

---
+++ API/DB/opMysql.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+# 
+#  FileName     : opMysql.py
+#  Date         : 2020年08月26日
+#  Description  : 
+# 
---
+++ API/DB/opRedis.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+# 
+#  FileName     : opRedis.py
+#  Date         : 2020年08月26日
+#  Description  : 
+# 
